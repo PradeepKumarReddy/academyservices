44e64ddca3fb454554b3c0361c05acd505fe455a
LAPTOP-B4TVL8KA\Pradeep <narreddyp@gmail.com>
2018-05-15 13:17:14 +0530

    jwt implimentation

---
+++ services/src/main/resources/sql/niadb_ddl.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA `niadb` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `application_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
